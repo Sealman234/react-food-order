8215008bddc94e294f13cbdd2e17240be9d7c53b
Sealman <castle2668@gmail.com>
2021-11-02 00:28:47 +0800

    新增 Cart Modal with Portals

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Modal from '../UI/Modal';
+
 import classes from './Cart.module.css';
 
 const Cart = () => {
@@ -12,18 +14,17 @@ const Cart = () => {
   );
 
   return (
-    <div>
+    <Modal>
       {cartItem}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
-      <div className={classes.actions}>Close</div>
-      <div>
+      <div className={classes.actions}>
         <button className={classes['button--alt']}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
